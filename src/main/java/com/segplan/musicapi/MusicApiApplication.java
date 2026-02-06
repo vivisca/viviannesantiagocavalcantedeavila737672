cb3af427bc0bd0cd2d95554f4ed16c8a76b232a9
vivisca <vivisc06@gmail.com>
2026-02-05 21:48:11 -0300

    adicionando a classe principal

---
+++ src/main/java/com/segplan/musicapi/MusicApiApplication.java
@@ -0,0 +1,12 @@
+﻿package com.segplan.musicapi;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class MusicApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MusicApiApplication.class, args);
+    }
+}
